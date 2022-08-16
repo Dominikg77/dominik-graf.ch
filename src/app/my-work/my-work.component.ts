76c45f6b73887255198c5eb020c1f07628af961d
Dominik <dominik.graf2001@gmail.com>
2022-08-16 14:56:54 +0200

    function my work button

--- src/app/my-work/my-work.component.ts
+++ src/app/my-work/my-work.component.ts
@@ -11,5 +11,44 @@ export class MyWorkComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  all = true;
+  angular = true;
+  java = true;
+
+  jsProjects = false;
+  angProjects = false;
+
+  pokedox = false;
+  game = false;
+  join = false;
+  ringoffire = false;
+  website = false
+  weather = false;
+  allProjects() {
+    this.all = true;
+    this.java = true;
+    this.angular = true;
+    this.jsProjects = false;
+    this.angProjects = false;
+
+  }
+  angularProjects() {
+    this.angular = true;
+    this.angProjects = true;
+    this.all = false;
+    this.java = false;
+    this.jsProjects = false;
+
+
+  }
+
+  javaProjects() {
+    this.java = true
+    this.jsProjects = true
+    this.all = false;
+    this.angular = false;
+    this.angProjects = false;
+
+  }
 
 }
