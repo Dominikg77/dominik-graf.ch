38adf4565e281b308591e158d8bac4ded42d0004
Dominik <dominik.graf2001@gmail.com>
2022-08-16 18:32:30 +0200

    start e-mail

--- src/app/contact-me/contact-me.component.ts
+++ src/app/contact-me/contact-me.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-me',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-me.component.scss']
 })
 export class ContactMeComponent implements OnInit {
+  isSubmitted = false;
+
+
+  public email: FormGroup = new FormGroup({
+    name: new FormControl('', [
+      Validators.required
+    ], []),
+    email: new FormControl('', [
+      Validators.required
+    ], []),
+    message: new FormControl('', [
+      Validators.required
+    ], []),
+
+  });
+
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  onSubmit() {
 
+    setTimeout(() => {
+      this.isSubmitted = true;
+    }, 3000);
+    
+}
 }
