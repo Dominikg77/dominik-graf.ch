fa8209457784d8d9c3ef1bd95ce22a5a58159b93
Dominik <dominik.graf2001@gmail.com>
2022-08-19 20:12:20 +0200

    arrow up function

--- src/app/back-top/back-top.component.ts
+++ src/app/back-top/back-top.component.ts
@@ -6,19 +6,26 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./back-top.component.scss']
 })
 export class BackTopComponent implements OnInit {
+idOnTop=false;
 
   constructor() { }
 
   ngOnInit(): void {
+    document.getElementById('back-top2')?.classList.add('d-none');
   }
-  arrowRight: boolean=true;
-  
-window(){
-  if (window.scrollY <100){
-    this.arrowRight= false;
-    window.alert("funktion")
-  }else{
-    this.arrowRight= true;
-  }
+ 
 }
+
+addEventListener('scroll', function(){
+let isOnTop=true;
+console.log(scrollY)
+
+if(isOnTop && this.scrollY <= 750){
+  console.log('oben')
+  this.document.getElementById('back-top2')?.classList.add('d-none')
+}else{
+  this.document.getElementById('back-top2')?.classList.remove('d-none')
+  isOnTop = false;
+  console.log('nicht ganz oben')
 }
+});
