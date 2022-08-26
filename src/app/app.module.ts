85e9bc6725458a7214eba7c52cdcf4eeb657ced5
Dominik <dominik.graf2001@gmail.com>
2022-08-26 19:19:16 +0200

    translate bug fixing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,8 +21,9 @@ import { BackTopComponent } from './back-top/back-top.component';
 import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
+
 export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http);
+  return new TranslateHttpLoader(http, "assets/i18n/", ".json");
 }
 
 @NgModule({
